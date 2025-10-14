1a9a0db880546bcdd29dc0f52383cd1ca9a5226a
Your Name <you@example.com>
2025-10-14 23:04:10 +0545

    first

---
+++ profile collector.py
@@ -0,0 +1,37 @@
+# name = input(" Provide ur name : ")
+# age = int(input("Provide ur age : "))
+# hobby = input("Provide ur hobby : ")
+# print (f"Hello {name}! You are {age} years old and in next year you will be {age+1} and you like to {hobby}")
+
+
+# print ("=== ToDo List ===")
+
+# list = []
+
+# print ("Your today list")
+
+# while True:
+
+
+#     options = int(input("Enter 1 to view tasks , 2 to add task, 3 to remove task or 4 to close list : "))
+#     if (options == 1):
+#         for i in list:
+#             print (i)
+
+#     elif (options == 2):
+#         item = input("add task : ")
+#         list.append(item)
+
+#     elif ( options ==3):
+#         item = input("Remove task : ")
+#         list.remove(item)
+
+#     elif (options == 4):
+#         print ("Closing ...")
+
+#         break
+
+#     else:
+#         print ("Enter valid number...")
+
+        
